663b15505329347f5baa00feb82d261e12cd8113
Anastasiia <che6yforever@gmail.com>
2016-09-15 16:37:39 +0700

    everything work

--- src/components/app.js
+++ src/components/app.js
@@ -2,17 +2,51 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import DoneItem from './done_item';
 
+let start, end;
 export default class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			item: this.props.item
+			item: this.props.item,
+			count: 0,
+			timeUp: "00:00:00"
 		}
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+	onInputChange(it, newTitle, newRate) {
+		var item = this.props.item;
+		var i = item.indexOf(it);
+		item[i].title = newTitle;
+		item[i].rate = newRate;
+		localStorage.setItem('item', JSON.stringify(item));
+		this.setState({ item: item});
+   }
+	ticking () {
+		var sec = this.state.count;
+		var h = Math.floor(sec/3600);
+		var m = Math.floor(sec%3600/60);
+		var s = Math.floor(sec%3600%60);
+		var timeUp = ((h > 0 ? h:"00" + ":" + (m < 10 ? "0" : "0") : "") + m + ":" + (s < 10 ? "0" : "") + s);
+		this.setState({count: this.state.count + 1, timeUp: timeUp});
+	}
+	startTimer () {
+		clearInterval(this.timer);
+		this.timer = setInterval(this.ticking.bind(this), 1000);
+	}
+	stopTimer () {
+		clearInterval(this.timer);
 	}
    addTask() {
 		var item = this.props.item;
-		item.push(ReactDOM.findDOMNode(this.refs.myInput).value);
-		ReactDOM.findDOMNode(this.refs.myInput).value = "";
+		var i = {
+			title: ReactDOM.findDOMNode(this.refs.taskName).value,
+			rate: ReactDOM.findDOMNode(this.refs.taskRate).value,
+			count: this.state.count
+		};
+		item.push(i);
+		ReactDOM.findDOMNode(this.refs.taskName).value = "";
+		ReactDOM.findDOMNode(this.refs.taskRate).value = "";
+		this.setState({ count: 0, timeUp: "00:00:00"});
 		localStorage.setItem('item', JSON.stringify(item));
 		this.setState({ item:item});
 	}
@@ -25,23 +59,60 @@ export default class App extends Component {
 	onFormSubmit(event) {
    	event.preventDefault();
   	}
+	onClickBtn() {
+		const btn = document.getElementById('start-stop-btn');
+		if (btn.getAttribute("data-text-swap") == btn.innerHTML) {
+			btn.innerHTML = btn.getAttribute("data-text-original");
+			this.stopTimer();
+			this.addTask();
+		} else {
+	      btn.setAttribute("data-text-original", btn.innerHTML);
+	      btn.innerHTML = btn.getAttribute("data-text-swap");
+			this.startTimer();
+    	}
+	}
    render() {
 		return (
       <div>
 			<h1>Tasks: {this.props.item.length}</h1>
-		  	<form className="input-group" onSubmit={this.onFormSubmit}>
-				<input
-					type="text"
-					className="form-control"
-					placeholder="Task name here..."
-					ref="myInput" />
-				<div className="input-group-btn">
-					<button className="btn btn-secondary" onClick={this.addTask.bind(this)}>Add</button>
- 				</div>
+		  	<form className="form-inline" onSubmit={this.onFormSubmit}>
+				<div className="form-group">
+					<label className="sr-only" htmlFor="task-name-input">Task Name</label>
+					<input
+						type="text"
+						className="form-control"
+						placeholder="Task name here..."
+						ref="taskName"
+						id="task-name-input"/>
+    			</div>
+				<div className="form-group">
+					<label className="sr-only" htmlFor="task-price-input">Task Price</label>
+					<input
+						type="number"
+						className="form-control"
+						placeholder="RUB"
+						ref="taskRate"
+						aria-describedby="basic-addon"
+						id="task-price-input"/>
+    			</div>
+				<div className="form-group">
+				<div>{this.state.timeUp}</div>
+				</div>
+				<button
+					id="start-stop-btn"
+					className="btn btn-secondary"
+					data-text-swap="Stop"
+					onClick={this.onClickBtn.bind(this)}>Start</button>
    		</form>
 			<ul className="list-group">
-				{this.state.item.map(function(task) {
-					return <DoneItem task={task} doneTasks={this.doneTasks.bind(this)} />
+				{this.state.item.map(function(i) {
+					return <DoneItem
+								title={i.title}
+								rate={i.rate}
+								count={i.count}
+								i= {i}
+								onInputChange = {this.onInputChange}
+								doneTasks={this.doneTasks.bind(this)} />
 				}.bind(this))}
 			</ul>
 		</div>
--- src/components/done_item.js
+++ src/components/done_item.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 export default class DoneItem extends Component {
 	doneTasks() {
-		this.props.doneTasks(this.props.task);
+		this.props.doneTasks(this.props.i);
+	}
+	update() {
+		var newTitle = ReactDOM.findDOMNode(this.refs.newTitle).value;
+		var newRate = ReactDOM.findDOMNode(this.refs.newRate).value;
+      this.props.onInputChange(this.props.i, newTitle, newRate);
 	}
 	render() {
+		const {title, rate, count} = this.props;
+		let hours = count/3600;
+		var h = Math.floor(count/3600);
+		var m = Math.floor(count%3600/60);
+		var s = Math.floor(count%3600%60);
+		var timeSpended = ((h > 0 ? h:"00" + ":" + (m < 10 ? "0" : "0") : "") + m + ":" + (s < 10 ? "0" : "") + s);
+		const price = Math.round(hours*rate * 10) / 10;
+
    	return (
 			<li className="list-group-item clearfix">
-				{this.props.task}
+				<input ref="newTitle" type="text" value={title} onChange={this.update.bind(this)}/>
+				<input ref="newRate" type="number" value={rate} onChange={this.update.bind(this)}/>
+				<div>{price} RUB</div>
+				<div>Time spended: {timeSpended}</div>
 				<div className="pull-right" role="group">
-				<button type="button" className="btn btn-xs btn-danger img-circle" onClick={this.doneTasks.bind(this)}>&#xff38;</button>
+					<button type="button" className="btn btn-xs btn-danger img-circle" onClick={this.doneTasks.bind(this)}>&#xff38;</button>
 				</div>
 			</li>
 		);
